2087548752fc47962cc380ab9043334056ab1eac
Aurélien Goulon <aurelien@learnpine.com>
2022-02-16 22:37:04 +0100

    Add db connector to games GET route

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../db/conn');
+console.log(db.getDb());
+
 /* GET game listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with all game listings');
+router.get('/', async function (req, res, next) {
+  const dbConnect = db.getDb();
+  console.log(dbConnect);
+  dbConnect
+    .collection('gameListings')
+    .find({})
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings.');
+      } else {
+        res.json(result);
+      }
+    })
 });
 
 router.post('/', function (req, res, next) {
